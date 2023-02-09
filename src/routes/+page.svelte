6a41ebd4a9fe31036cbad69c83bf65f25d27318c
Vishwa Gaurav <81325730+VishwaGauravIn@users.noreply.github.com>
2023-02-09 17:21:23 +0530

    Svelte+TailwindCSS setup

---
+++ src/routes/+page.svelte
@@ -0,0 +1,14 @@
+<script>
+	let clickCount = 0;
+	const onClickFun = () => {
+		clickCount++;
+	};
+</script>
+
+<h1>Welcome to SvelteKit Starter Template superpowered by TailwindCSS</h1>
+<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+
+<button class="bg-yellow-200 text-yellow-900 px-6 py-2 font-semibold m-4" on:click={onClickFun}
+	>Click Me</button
+>
+{clickCount}
---
+++ src/routes/test/+page.svelte
@@ -0,0 +1,1 @@
+<html lang="en" class=""> test </html>
